840dbb5a59cc67e43f31fdfc5efd861d6948b329
tancang91 <tancang91@gmail.com>
2022-11-06 20:59:17 +0700

    Draw rectange

--- visort/src/main.rs
+++ visort/src/main.rs
@@ -1,11 +1,28 @@
-use bevy::prelude::*;
-
-fn hello_world() {
-    println!("Hello World");
-}
+use bevy::{prelude::*, sprite::Anchor};
 
 fn main() {
     App::new()
-        .add_system(hello_world)
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands
+) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    // Rectangle
+    commands.spawn_bundle(SpriteBundle {
+        transform: Transform::from_translation(
+                       Vec3::new(-200.0, 200.0, 0.0)
+        ),
+        sprite: Sprite {
+            color: Color::RED,
+            custom_size: Some(Vec2::new(50.0, 20.0)),
+            anchor: Anchor::TopLeft,
+            ..default()
+        },
+        ..default()
+    });
 }
