5603b4f1cf883619f5ba3fdc6378b6a43a546e23
tancang91 <tancang91@gmail.com>
2022-11-19 19:47:35 +0700

    Refactor sorting_system

--- visort/src/main.rs
+++ visort/src/main.rs
@@ -1,11 +1,11 @@
-use bevy::{prelude::*, sprite::Anchor};
 use bevy::time::FixedTimestep;
+use bevy::{prelude::*, sprite::Anchor};
 use rand::seq::SliceRandom;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
 // Default width
-const BAR_HEIGH: f32 = 10.0;
+const BAR_HEIGH: f32 = 6.0;
 
 // Window setting
 const WINDOW_WIDTH: f32 = 1000.0;
@@ -15,9 +15,9 @@ const WINDOW_PADDING: f32 = 5.0;
 // Bar setting
 const BAR_COLOR: Color = Color::RED;
 const BAR_PADDING: f32 = 4.0;
-const NUMBER_BARS: u8 = 10;
+const NUMBER_BARS: u8 = 20;
 
-const TIMESTEP_1_PER_SECOND: f64 = 15.0 / 60.0;
+const TIMESTEP_1_PER_SECOND: f64 = 60.0 / 60.0;
 
 fn main() {
     App::new()
@@ -33,6 +33,7 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIMESTEP_1_PER_SECOND))
+                .with_system(render_system)
                 .with_system(sorting_system)
         )
         .run();
@@ -55,7 +56,7 @@ fn setup(mut commands: Commands, windows: Res<Windows>) {
 
     let mut range: Vec<f32> = (0..NUMBER_BARS)
         .into_iter()
-        .map(|w| width_base * (1.0 + 0.2 * w as f32))
+        .map(|w| width_base * (2.0 + 0.2 * w as f32))
         .collect();
 
     range.shuffle(&mut rng);
@@ -76,26 +77,56 @@ fn setup(mut commands: Commands, windows: Res<Windows>) {
     commands.insert_resource(bar_collection);
 }
 
-fn sorting_system(mut bar_collection: ResMut<BarCollection>
-    , mut query: Query<(&Bar, &mut Sprite, &mut Transform)>
-    , windows: Res<Windows>)
+fn is_sorted<I>(data: I) -> bool
+where
+    I: IntoIterator,
+    I::Item: Ord,
 {
-    let window = windows.get_primary().unwrap();
-    let end = (NUMBER_BARS - 1) as usize;
+    let mut it = data.into_iter();
+    match it.next() {
+        None => true,
+        Some(first) => it.scan(first, |state, next| {
+            let cmp = *state <= next;
+            *state = next;
+            Some(cmp)
+        }).all(|b| b),
+    }
+}
 
-    let bar_start = bar_collection.bars[0];
-    let bar_end = bar_collection.bars[end];
+fn sorting_system(mut bar_collection: ResMut<BarCollection>, bars: Query<&Bar>) {
+    let ranges: Vec<_> = bar_collection.bars.iter()
+        .map(|entity| bars.get(entity.clone()).unwrap().length as i32)
+        .collect();
 
-    bar_collection.bars.swap(0, end);
-
-    if let Ok((_, mut sprite, mut transform)) = query.get_mut(bar_start) {
-        transform.translation.y = rank_to_y(end as u32, window.height());
-        sprite.color = Color::BLUE;
+    if !is_sorted(ranges) {
+        bar_collection.bars.sort_by_key(|entity| {
+            if let Ok(bar) = bars.get(entity.clone()) {
+                bar.length as i32
+            } else {
+                -1
+            }
+        });
     }
 
-    if let Ok((_, mut sprite, mut transform)) = query.get_mut(bar_end) {
-        transform.translation.y = rank_to_y(0, window.height());
-        sprite.color = Color::DARK_GREEN;
+    // let mut rng = rand::thread_rng();
+    // bar_collection.bars.shuffle(&mut rng);
+}
+
+fn render_system(
+    bar_collection: ResMut<BarCollection>,
+    mut query: Query<(&Bar, &mut Sprite, &mut Transform)>,
+    windows: Res<Windows>,
+) {
+    if bar_collection.is_changed() {
+        let height = windows.get_primary().unwrap().height();
+
+        for i in 0..bar_collection.bars.len() {
+            let bar = bar_collection.bars[i];
+            if let Ok((_, mut sprite, mut transform)) = query.get_mut(bar) {
+                transform.translation.y = rank_to_y(i as u32, height);
+                sprite.color = Color::BLUE;
+            }
+        }
     }
 }
 
@@ -118,7 +149,7 @@ struct BarBunndle {
 impl BarBunndle {
     fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
         Self {
-            bar: Bar {length: width},
+            bar: Bar { length: width },
             sprite_bundle: SpriteBundle {
                 transform: Transform::from_translation(Vec3::new(x, y, 0.0)),
                 sprite: Sprite {
@@ -137,4 +168,3 @@ fn rank_to_y(rank: u32, y_base: f32) -> f32 {
     let y_base: f32 = y_base / 2.0 - WINDOW_PADDING;
     y_base - (rank as f32 * (BAR_HEIGH + BAR_PADDING))
 }
-
